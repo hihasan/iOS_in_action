9dc334dd9409cec040f115bc2b0d932c64113462
hihasan <hihasan07@gmail.com>
2023-05-11 02:21:16 +0600

    practicing Swift UI

---
+++ ios_in_action/ios_in_action/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  ios_in_action
+//
+//  Created by Hasan Al Mamun on 11/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack{
+            Text("Hasan")
+            Text("Al Mamun")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios_in_action/ios_in_action/ios_in_actionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_in_actionApp.swift
+//  ios_in_action
+//
+//  Created by Hasan Al Mamun on 11/5/23.
+//
+
+import SwiftUI
+
+@main
+struct ios_in_actionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
