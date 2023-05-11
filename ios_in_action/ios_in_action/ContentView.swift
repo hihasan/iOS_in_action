9eed711edaca890d7f2a3bc3abb1ef2f0ac72df6
Hasan Al Mamun <hihasan07@gmail.com>
2023-05-11 18:57:33 +0600

    Content View Navigation view

--- ios_in_action/ios_in_action/ContentView.swift
+++ ios_in_action/ios_in_action/ContentView.swift
@@ -8,11 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        HStack{
-            Text("Hasan")
-            Text("Al Mamun")
+    
+        NavigationView{
+            Screen(title: "Home", text: "First Screen", imageName: "house")
         }
+        
+    }
+}
+
+struct Screen : View{
+    let title: String
+    let text: String
+    let imageName : String
+    
+    var body: some View{
+        VStack{
+            Image(systemName: imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width:200,
+                       height: 200,
+                       alignment: .center)
+                .padding()
+            
+            Text(text).font(.system(size: 30, weight: .light, design: .default)).padding()
+            
+            
+            NavigationLink(
+                destination: Screen(title: "Notification",
+                                    text: "Notification Screen",
+                                    imageName: "bell"),
+                label: {
+                    ContinueButton()
+                }
+            )
+        }.navigationTitle(title)
+    }
+}
+
+struct ContinueButton : View{
+    var body: some View{
+    
+        Text("Continue")
+            .frame(width: 200, height: 50, alignment: .center)
+            .background(Color.pink)
+            .foregroundColor(Color.white)
+            .cornerRadius(15)
     }
 }
 
