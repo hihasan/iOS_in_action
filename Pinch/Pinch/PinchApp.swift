53e5a9e7250e3d7b60d786c5cab16f11c98e1037
Hasan Al Mamun <hihasan07@gmail.com>
2023-06-14 16:14:22 +0600

    System files

---
+++ Pinch/Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Appnap ws27 on 14/6/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
