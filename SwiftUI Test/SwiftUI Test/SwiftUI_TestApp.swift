ffcad6fac6f177c7815f4c3544e4dee9a0b6ef28
hihasan <86922459+hihasan07@users.noreply.github.com>
2023-07-17 23:54:12 +0600

    Lazy V Grid

---
+++ SwiftUI Test/SwiftUI Test/SwiftUI_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TestApp.swift
+//  SwiftUI Test
+//
+//  Created by Hasan Al Mamun on 17/7/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
